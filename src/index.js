cd071b78f6ac2cc7142a2c9e21b20dceac61858f
HoustonSprouse919 <houstonsprouse@gmail.com>
2022-10-12 13:41:09 -0400

    barebones buttons for tab navigation added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+(function buttons() {
+    let button1 = document.createElement("button");
+    button1.append("Home");
+    button1.addEventListener("click", changePage);
+    document.querySelector('#tabs').appendChild(button1);
+
+    let button2 = document.createElement("button");
+    button2.append("Menu");
+    button2.addEventListener("click", changePage);
+    document.querySelector('#tabs').appendChild(button2);
+
+    let button3 = document.createElement("button");
+    button3.append("Contact");
+    button3.addEventListener("click", changePage);
+    document.querySelector('#tabs').appendChild(button3);
+
+   })();
+   function changePage(){
+    console.log("THis is working")
+}
 console.log("This is working!")
-import { component } from "./homePage"
-component();
+import { h1, image, paragraph1 } from "./homePage"
